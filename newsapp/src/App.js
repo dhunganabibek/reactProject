9adf47896e4808bf4c28e3f41c922bbde245de00
Bibek Dhungana <bibek.dhungana@hcss.com>
2022-06-11 21:10:03 -0500

    news app-- class based component added

---
+++ newsapp/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import './App.css';
+
+export default class App extends Component{
+  render(){
+    return(
+      <>
+        <h1>Hello world</h1>
+      </>
+    )
+
+  }
+}
