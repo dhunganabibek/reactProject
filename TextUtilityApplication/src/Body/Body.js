0070db17a05091e2d93c843f91d7554b4b5c0252
KiritiAryal <karyal@ttu.edu>
2022-09-24 12:28:08 -0500

    Text to TextForm in Body.js

--- TextUtilityApplication/src/Body/Body.js
+++ TextUtilityApplication/src/Body/Body.js
@@ -1,15 +1,13 @@
-import './Body.css'
-import React from 'react'
-import TextForm from './../TextForm/TextForm'
-
+import "./Body.css";
+import React from "react";
+import TextForm from "./../TextForm/TextForm";
 
 export default function Body() {
   return (
     <>
-        <div className="container">
-            <TextForm heading="Enter the Text"></TextForm>
-        </div>
+      <div className="container">
+        <TextForm heading="Enter the TextForm"></TextForm>
+      </div>
     </>
-    
-  )
+  );
 }
